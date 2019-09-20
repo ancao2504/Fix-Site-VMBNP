a9b642dd6ae5cca84cca2da79334cefe1da5a78f
ancao2504 <pullnolovesmile@gmail.com>
2019-09-20 09:32:21 +0700

    Update File Config Language HTML

--- funtions.php
+++ funtions.php
@@ -1,16 +1,25 @@
 <?php
-// FIX AUTOPTIMIZE CACHE BIG SIZE 
-if (class_exists('autoptimizeCache')) {
-  $siteMaxSize = 2000000;
-  $statArr = autoptimizeCache::stats();
-  $cacheSize = round($statArr[1]/1024);
+	// FIX AUTOPTIMIZE CACHE BIG SIZE 
+	if (class_exists('autoptimizeCache')) {
+	  $siteMaxSize = 2000000;
+	  $statArr = autoptimizeCache::stats();
+	  $cacheSize = round($statArr[1]/1024);
 
-  if ($cacheSize > $siteMaxSize){
-     autoptimizeCache::clearall();
-     header("Refresh:0");
-  }
-}
+	  if ($cacheSize > $siteMaxSize){
+	     autoptimizeCache::clearall();
+	     header("Refresh:0");
+	  }
+	}
 
+	// TÙY CHỈNH NGÔN NGỮ WEBSITE
+	add_filter('language_attributes', 'custom_lang_attr');
+	function custom_lang_attr() {
+	    return 'lang="vi-VN"';
+	}
 
+	add_filter('wpseo_locale', 'override_og_locale');
+	function override_og_locale($locale) {
+	    return "vi_VN";
+	}
 
 ?>
